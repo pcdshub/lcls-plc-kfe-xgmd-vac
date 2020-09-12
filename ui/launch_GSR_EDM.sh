fc765406671a72065369c5865ccd3a7d8cf24e8e
Nolan W. Brown <n-wbrown@users.noreply.github.com>
2020-09-11 21:43:43 -0700

    BUG: load script was subject to users pwd

--- ui/launch_GSR_EDM.sh
+++ ui/launch_GSR_EDM.sh
@@ -1,2 +1,6 @@
+#!/bin/bash
+
 source /reg/g/pcds/setup/epicsenv-3.15.5-2.0.sh
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+pushd $DIR
 edm -x -noedit -m P=EM2K0:XGMD:GSR:1 srg3.edl &
